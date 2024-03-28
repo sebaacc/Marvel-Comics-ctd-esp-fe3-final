bd975795a157a88e39c86fdd07d3fee0937007cc
Sebastián-Markoja <sebaacc11@gmail.com>
2024-03-28 20:54:50 -0300

    checkout cambios

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -2,13 +2,8 @@ import type { AppProps } from "next/app";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import LayoutGeneral from "dh-marvel/components/layouts/layout-general";
 import { theme } from "dh-marvel/styles/material-theme";
-import LayoutCheckout from "dh-marvel/components/layouts/layout-checkout";
-import CheckoutPage from "./checkout/[id].page";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const isCheckout =
-    Component["displayName"] === "CheckoutPage" || "ConfirmationPage"; // Verificar si el componente es CheckoutPage
-
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
@@ -16,7 +11,6 @@ function MyApp({ Component, pageProps }: AppProps) {
       <LayoutGeneral>
         <Component {...pageProps} />
       </LayoutGeneral>
-
       <style jsx global>{`
         /* Other global styles such as 'html, body' etc... */
 
